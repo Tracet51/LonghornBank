fea78a9f95c03af37e9b221e292671c03da4500d
Trace Tschida <trace@tschida.us>
2017-03-30 19:41:31 -0500

    deleted comments

--- LonghornBank/Models/Account.cs
+++ LonghornBank/Models/Account.cs
@@ -8,13 +8,5 @@ namespace LonghornBank.Models
     public class Account
     {
         public Int32 AccountID { get; set; }
-
-        // this is a test 
-        // this is a further test
-        
-
-        // this is a test
-
-        // test against!!!!!
     }
 }
