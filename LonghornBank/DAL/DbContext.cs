ce8c8903eb743212b1202a9a8328041530449c36
Trace Tschida <trace@tschida.us>
2017-03-31 11:25:46 -0500

    Created DAL and Update Model

---
+++ LonghornBank/DAL/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using LonghornBank.Models;
+
+namespace LonghornBank.Dal
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("MyDBConnection") { }
+
+        public DbSet<Checking> CheckingAccount { get; set; }
+
+
+    }
+}
--- LonghornBank/Models/Checking.cs
+++ LonghornBank/Models/Checking.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using LonghornBank.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace LonghornBank.Models
 {
