c4c3ec0bb1809253e9e93bf0964510031473b479
Trace Tschida <trace@tschida.us>
2017-03-31 22:00:47 -0500

    Link to the portal

--- LonghornBank/Controllers/HomeController.cs
+++ LonghornBank/Controllers/HomeController.cs
@@ -34,7 +34,28 @@ namespace LonghornBank.Controllers
             {
                 return HttpNotFound();
             }
-            return View();
+
+            // Find the Checking Accounts Associated
+            var CheckingQuery = from ca in db.CheckingAccount
+                                where ca.Customer.CustomerID == (Int32)id
+                                select ca;
+
+            // Convert to a list and execute
+            List <Checking> CustomerCheckings = CheckingQuery.ToList();
+
+            // Send to the Viewbag
+            ViewBag.CheckingAccounts = CustomerCheckings;
+
+            // Find the Savings Accounts Associated 
+            var SavingsQuery = from sa in db.SavingsAccount
+                               where sa.Customer.CustomerID == (Int32)id
+                               select sa;
+
+            // Convert to a list and execute 
+            List<Saving> CustomerSavings = SavingsQuery.ToList();
+            ViewBag.SavingsAccounts = CustomerSavings;
+
+            return View(customer);
         }
     }
 }
