b8615f0ebb8473540a83eea35663eb0877ea2e8c
Carson <carsonparks12@yahoo.com>
2017-04-06 18:37:16 -0500

    Start identity

---
+++ LonghornBank/Models/RoleViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace IdentityTemplate.Models
+{
+    public class RoleEditModel
+    {
+        public AppRole Role { get; set; }
+        public IEnumerable<AppUser> Members { get; set; }
+        public IEnumerable<AppUser> NonMembers { get; set; }
+    }
+
+    public class RoleModificationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
+}
