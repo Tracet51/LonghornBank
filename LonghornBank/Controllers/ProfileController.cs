5ea29aa301b63ff19a598180e8358d011e8f1816
Trace Tschida <trace@tschida.us>
2017-04-28 21:34:52 -0500

    Password Reset

--- LonghornBank/Controllers/ProfileController.cs
+++ LonghornBank/Controllers/ProfileController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using LonghornBank.Models;
+using LonghornBank.Utility;
 
 namespace LonghornBank.Controllers
 {
@@ -266,12 +267,13 @@ namespace LonghornBank.Controllers
                     // The Birday years do not match up
                     return View("ForgotPasswordConfirmation");
                 }
-
+                  
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
                 string code = await UserManager.GeneratePasswordResetTokenAsync(user.Id);
                 var callbackUrl = Url.Action("ResetPassword", "Profile", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);		
-                await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                //await UserManager.SendEmailAsync(user.Id, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
+                Email.PasswordEmail(user.Email, "Reset Password", "Please reset your password by clicking <a href=\"" + callbackUrl + "\">here</a>");
                 return RedirectToAction("ForgotPasswordConfirmation", "Account");
             }
 
--- LonghornBank/Controllers/SavingsController.cs
+++ LonghornBank/Controllers/SavingsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using LonghornBank.Models;
+using LonghornBank.Utility;
 
 namespace LonghornBank.Controllers
 {
