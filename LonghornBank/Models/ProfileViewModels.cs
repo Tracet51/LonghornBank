8b9aad59bdf809877a291273fcbe63ddb2e19082
Trace Tschida <trace@tschida.us>
2017-04-28 23:35:44 -0500

    Password Reset Working

--- LonghornBank/Models/ProfileViewModels.cs
+++ LonghornBank/Models/ProfileViewModels.cs
@@ -98,8 +98,6 @@ namespace LonghornBank.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
-
-        public string Code { get; set; }
     }
 
     public class ForgotPasswordViewModel
