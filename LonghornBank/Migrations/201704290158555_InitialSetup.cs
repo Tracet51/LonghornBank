ce339cd72255796b516bee199d8a8b4c857d52c8
Carson <carsonparks12@yahoo.com>
2017-04-28 22:16:59 -0500

    depos

---
+++ LonghornBank/Migrations/201704290158555_InitialSetup.cs
@@ -0,0 +1,64 @@
+namespace LonghornBank.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialSetup : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Payees",
+                c => new
+                    {
+                        PayeeID = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        StreetAddress = c.String(nullable: false),
+                        City = c.String(nullable: false),
+                        State = c.String(nullable: false),
+                        Zip = c.String(),
+                        PhoneNumber = c.String(nullable: false),
+                        PayeeType = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.PayeeID);
+            
+            CreateTable(
+                "dbo.PayeeAppUsers",
+                c => new
+                    {
+                        Payee_PayeeID = c.Int(nullable: false),
+                        AppUser_Id = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.Payee_PayeeID, t.AppUser_Id })
+                .ForeignKey("dbo.Payees", t => t.Payee_PayeeID, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetUsers", t => t.AppUser_Id, cascadeDelete: true)
+                .Index(t => t.Payee_PayeeID)
+                .Index(t => t.AppUser_Id);
+            
+            AddColumn("dbo.IRAs", "Fee", c => c.Decimal(nullable: false, precision: 18, scale: 2));
+            AddColumn("dbo.StockAccounts", "AccountNumber", c => c.String());
+            AlterColumn("dbo.Checkings", "AccountNumber", c => c.String());
+            AlterColumn("dbo.Checkings", "Name", c => c.String());
+            AlterColumn("dbo.Savings", "Name", c => c.String());
+            AlterColumn("dbo.Savings", "AccountNumber", c => c.String());
+            AlterColumn("dbo.StockAccounts", "Name", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.PayeeAppUsers", "AppUser_Id", "dbo.AspNetUsers");
+            DropForeignKey("dbo.PayeeAppUsers", "Payee_PayeeID", "dbo.Payees");
+            DropIndex("dbo.PayeeAppUsers", new[] { "AppUser_Id" });
+            DropIndex("dbo.PayeeAppUsers", new[] { "Payee_PayeeID" });
+            AlterColumn("dbo.StockAccounts", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Savings", "AccountNumber", c => c.String(nullable: false));
+            AlterColumn("dbo.Savings", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Checkings", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Checkings", "AccountNumber", c => c.String(nullable: false));
+            DropColumn("dbo.StockAccounts", "AccountNumber");
+            DropColumn("dbo.IRAs", "Fee");
+            DropTable("dbo.PayeeAppUsers");
+            DropTable("dbo.Payees");
+        }
+    }
+}
